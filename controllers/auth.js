149be5f1f6f68739a69e9f839222e3037b580e5e
Jottapeg <151564769+Jottapeg@users.noreply.github.com>
2025-04-26 02:10:48 -0300

    Update auth.js

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,4 +1,4 @@
-const User = require('../models/User');
+const Usuario = require('../models/Usuario');
 const ErrorResponse = require('../utils/errorResponse');
 const asyncHandler = require('../middleware/async');
 const sendEmail = require('../utils/sendEmail');
