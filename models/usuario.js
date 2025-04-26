063a059cf6e626903c21b0c020eb1e3a39e43b6e
Jottapeg <151564769+Jottapeg@users.noreply.github.com>
2025-04-26 17:23:49 -0300

    Update usuario.js

--- models/usuario.js
+++ models/usuario.js
@@ -1,10 +1,27 @@
 const mongoose = require('mongoose');
 
 const UsuarioSchema = new mongoose.Schema({
-  nome: String,
-  email: String,
-  senha: String,
-  // adicione mais campos se quiser
+  nome: {
+    type: String,
+    required: [true, 'Por favor, adicione um nome']
+  },
+  email: {
+    type: String,
+    required: [true, 'Por favor, adicione um email'],
+    unique: true,
+    match: [
+      /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
+      'Por favor, adicione um email válido'
+    ]
+  },
+  senha: {
+    type: String,
+    required: [true, 'Por favor, adicione uma senha'],
+    minlength: 6,
+    select: false // não retorna a senha por padrão
+  }
+}, {
+  timestamps: true // cria automaticamente createdAt e updatedAt
 });
 
 module.exports = mongoose.model('Usuario', UsuarioSchema);
