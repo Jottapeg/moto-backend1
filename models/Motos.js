f40a6e0fe1004b897a736f793a20b93e525e2707
Jottapeg <151564769+Jottapeg@users.noreply.github.com>
2025-04-28 19:28:31 -0300

    Create Motos.js

---
+++ models/Motos.js
@@ -0,0 +1,59 @@
+const mongoose = require('mongoose');
+
+const MotoSchema = new mongoose.Schema(
+  {
+    usuario: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'Usuario',
+      required: true,
+    },
+    titulo: {
+      type: String,
+      required: [true, 'Por favor, adicione um título para o anúncio'],
+      trim: true,
+      maxlength: 100,
+    },
+    descricao: {
+      type: String,
+      required: [true, 'Por favor, adicione uma descrição'],
+      maxlength: 1000,
+    },
+    preco: {
+      type: Number,
+      required: [true, 'Por favor, adicione o preço da moto'],
+    },
+    marca: {
+      type: String,
+      required: [true, 'Por favor, adicione a marca da moto'],
+    },
+    modelo: {
+      type: String,
+      required: [true, 'Por favor, adicione o modelo da moto'],
+    },
+    ano: {
+      type: Number,
+      required: [true, 'Por favor, adicione o ano de fabricação'],
+    },
+    quilometragem: {
+      type: Number,
+      required: [true, 'Por favor, adicione a quilometragem da moto'],
+    },
+    imagem: {
+      type: String,
+      default: 'no-photo.jpg', // Caso o usuário não envie imagem
+    },
+    destaque: {
+      type: Boolean,
+      default: false,
+    },
+    premium: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+module.exports = mongoose.model('Moto', MotoSchema);
