234296aab0c71c6f96f2526187160141afee879e
Jottapeg <151564769+Jottapeg@users.noreply.github.com>
2025-04-28 19:01:28 -0300

    Update usuario.js

--- routes/usuario.js
+++ routes/usuario.js
@@ -1,10 +1,10 @@
 const express = require('express');
-const bcryptjs = require('bcryptjs'); // Para comparar a senha
-const jwt = require('jsonwebtoken');  // Para gerar o token JWT
+const bcryptjs = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 const router = express.Router();
 const Usuario = require('../models/usuario');
 
-// Rota para criar um novo usuário
+// Cadastro de usuário
 router.post('/', async (req, res) => {
   try {
     const { nome, email, senha } = req.body;
@@ -17,30 +17,27 @@ router.post('/', async (req, res) => {
   }
 });
 
-// Rota para login (usuário)
+// Login do usuário
 router.post('/login', async (req, res) => {
   const { email, senha } = req.body;
 
   try {
-    // Verificar se o usuário existe
     const usuario = await Usuario.findOne({ email });
 
     if (!usuario) {
       return res.status(400).json({ success: false, error: 'Usuário não encontrado' });
     }
 
-    // Verificar se a senha está correta
     const isMatch = await bcryptjs.compare(senha, usuario.senha);
 
     if (!isMatch) {
       return res.status(400).json({ success: false, error: 'Senha incorreta' });
     }
 
-    // Gerar o token JWT
     const token = jwt.sign(
       { id: usuario._id, email: usuario.email },
       process.env.JWT_SECRET,
-      { expiresIn: process.env.JWT_EXPIRE }  // Token expira conforme a configuração no .env
+      { expiresIn: process.env.JWT_EXPIRE }
     );
 
     res.status(200).json({ success: true, token });
@@ -50,10 +47,10 @@ router.post('/login', async (req, res) => {
   }
 });
 
-// Rota para listar todos os usuários
+// Listar todos usuários
 router.get('/', async (req, res) => {
   try {
-    const usuarios = await Usuario.find(); // Busca todos os usuários
+    const usuarios = await Usuario.find();
     res.status(200).json({ success: true, usuarios });
   } catch (err) {
     console.error(err);
