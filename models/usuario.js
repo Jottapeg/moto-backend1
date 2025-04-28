3f043ca0ab242f5b5b8368d0f2179cee85a7f3f9
Jottapeg <151564769+Jottapeg@users.noreply.github.com>
2025-04-28 17:39:53 -0300

    Update usuario.js

--- models/usuario.js
+++ models/usuario.js
@@ -1,19 +1,19 @@
 const mongoose = require('mongoose');
+const bcryptjs = require('bcryptjs');
 
 const usuarioSchema = new mongoose.Schema({
-  nome: {
-    type: String,
-    required: true
-  },
-  email: {
-    type: String,
-    required: true,
-    unique: true
-  },
-  senha: {
-    type: String,
-    required: true
-  }
+  nome: { type: String, required: true },
+  email: { type: String, required: true, unique: true },
+  senha: { type: String, required: true }
 }, { timestamps: true });
+
+// Antes de salvar, vamos criptografar a senha
+usuarioSchema.pre('save', async function(next) {
+  if (!this.isModified('senha')) return next();
+
+  const salt = await bcryptjs.genSalt(10);
+  this.senha = await bcryptjs.hash(this.senha, salt);
+  next();
+});
 
 module.exports = mongoose.model('Usuario', usuarioSchema);
