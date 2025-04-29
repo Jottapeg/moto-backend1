6fd759137c6b388e8938a86231dd3a376000f9ac
Jottapeg <151564769+Jottapeg@users.noreply.github.com>
2025-04-29 20:22:26 -0300

    Update subscriptions.js

--- controllers/subscriptions.js
+++ controllers/subscriptions.js
@@ -1,7 +1,7 @@
 const asyncHandler = require('../middleware/async');
 const ErrorResponse = require('../utils/errorResponse');
 const Subscription = require('../models/Subscription');
-const usuario = require('../models/usuario');
+const usuario = require('../models/Usuario');
 const Payment = require('../models/Payment');
 
 // @desc    Obter todas as assinaturas do usuário
